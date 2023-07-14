bdb6e834f018d01aeae5afcbea3adef6f50f44fe
Wslt <76235453+CodeWslt@users.noreply.github.com>
2023-07-14 00:02:30 -0400

    Add files via upload

---
+++ App.xaml.cs
@@ -0,0 +1,31 @@
+﻿using Galaxy_Swapper_v2.Workspace.Properties;
+using Galaxy_Swapper_v2.Workspace.Views;
+using Serilog;
+using System.Windows;
+
+namespace Galaxy_Swapper_v2
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            Config.Initialize();
+            Output.Initialize();
+            Settings.Initialize();
+            SwapLogs.Initialize();
+            SwapData.Initialize();
+            UEFN.Initialize();
+            DLLS.Initialize();
+
+            Log.Information("Successfully initialized properties");
+
+            new MainView().ShowDialog();
+
+            Log.Information("Shutting down..");
+            Application.Current.Shutdown();
+        }
+    }
+}
