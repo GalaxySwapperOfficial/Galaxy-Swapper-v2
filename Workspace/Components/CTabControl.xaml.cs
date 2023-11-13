597c52ddab4727c579ea4b0effc25e599f7518c5
Maurice <maurice403298z2@gmail.com>
2023-11-13 18:06:59 +0100

    Better CTabControl

--- Workspace/Components/CTabControl.xaml.cs
+++ Workspace/Components/CTabControl.xaml.cs
@@ -1,6 +1,8 @@
-﻿using System.Windows;
+﻿using System;
+using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 
 namespace Galaxy_Swapper_v2.Workspace.Components
 {
@@ -9,40 +11,42 @@ namespace Galaxy_Swapper_v2.Workspace.Components
     /// </summary>
     public partial class CTabControl : UserControl
     {
+        private bool isSelected = false;
+
         public static readonly DependencyProperty IconProperty =
-            DependencyProperty.Register("Icon", typeof(ImageSource), typeof(CTabControl));
+            DependencyProperty.Register(nameof(Icon), typeof(ImageSource), typeof(CTabControl));
 
         public ImageSource Icon
         {
-            get { return (ImageSource)GetValue(IconProperty); }
-            set { SetValue(IconProperty, value); }
+            get => (ImageSource)GetValue(IconProperty);
+            set => SetValue(IconProperty, value);
         }
 
         public static readonly DependencyProperty IconClickedProperty =
-            DependencyProperty.Register("IconClicked", typeof(ImageSource), typeof(CTabControl));
+            DependencyProperty.Register(nameof(IconClicked), typeof(ImageSource), typeof(CTabControl));
 
         public ImageSource IconClicked
         {
-            get { return (ImageSource)GetValue(IconClickedProperty); }
-            set { SetValue(IconClickedProperty, value); }
+            get => (ImageSource)GetValue(IconClickedProperty);
+            set => SetValue(IconClickedProperty, value);
         }
 
         public static readonly DependencyProperty ColorProperty =
-            DependencyProperty.Register("Color", typeof(Brush), typeof(CTabControl), new PropertyMetadata(Properties.Colors.External_Brush));
+            DependencyProperty.Register(nameof(Color), typeof(Brush), typeof(CTabControl), new PropertyMetadata(Properties.Colors.External_Brush));
 
         public Brush Color
         {
-            get { return (Brush)GetValue(ColorProperty); }
-            set { SetValue(ColorProperty, value); }
+            get => (Brush)GetValue(ColorProperty);
+            set => SetValue(ColorProperty, value);
         }
 
         public static readonly DependencyProperty PresenceProperty =
-            DependencyProperty.Register("Presence", typeof(string), typeof(CNewsControl));
+            DependencyProperty.Register(nameof(Presence), typeof(string), typeof(CNewsControl));
 
         public string Presence
         {
-            get { return (string)GetValue(PresenceProperty); }
-            set { SetValue(PresenceProperty, value); }
+            get => (string)GetValue(PresenceProperty);
+            set => SetValue(PresenceProperty, value);
         }
 
         public CTabControl()
@@ -52,31 +56,63 @@ namespace Galaxy_Swapper_v2.Workspace.Components
 
         public void Tab_Default()
         {
-            this.Clicked.Visibility = Visibility.Hidden;
-            this.Default.Visibility = Visibility.Visible;
-            this.Color = Properties.Colors.External_Brush;
+            Clicked.Visibility = Visibility.Hidden;
+            Default.Visibility = Visibility.Visible;
+            Color = Properties.Colors.External_Brush;
+            isSelected = false;
+            AnimateSize(40, 8);
         }
 
         public void Tab_Clicked()
         {
-            this.Default.Visibility = Visibility.Hidden;
-            this.Clicked.Visibility = Visibility.Visible;
-            this.Color = Properties.Colors.Blue_Brush;
+            Default.Visibility = Visibility.Hidden;
+            Clicked.Visibility = Visibility.Visible;
+
+            var colorAnimation = new ColorAnimation
+            {
+                From = ((SolidColorBrush)Properties.Colors.External_Brush).Color,
+                To = ((SolidColorBrush)Properties.Colors.Blue_Brush).Color,
+                Duration = TimeSpan.FromMilliseconds(200),
+                EasingFunction = new QuadraticEase { EasingMode = EasingMode.EaseInOut }
+            };
+
+            var brush = new SolidColorBrush();
+            brush.BeginAnimation(SolidColorBrush.ColorProperty, colorAnimation);
+
+            Color = brush;
+
             Utilities.Presence.Update(Presence);
+            AnimateSize(45, 7);
+            isSelected = true;
         }
 
         private void CTabControl_MouseEnter(object sender, System.Windows.Input.MouseEventArgs e)
         {
-            this.Width = 42;
-            this.Height = 42;
-            this.Margin = new Thickness(7);
+            AnimateSize(45, 7);
         }
 
         private void CTabControl_MouseLeave(object sender, System.Windows.Input.MouseEventArgs e)
         {
-            this.Width = 40;
-            this.Height = 40;
-            this.Margin = new Thickness(8);
+            if (!isSelected)
+            {
+                AnimateSize(40, 8);
+            }
+        }
+
+        private void AnimateSize(int size, int margin)
+        {
+            var sizeAnimation = new DoubleAnimation(size, TimeSpan.FromMilliseconds(200))
+            {
+                EasingFunction = new QuadraticEase { EasingMode = EasingMode.EaseInOut }
+            };
+            var marginAnimation = new ThicknessAnimation(new Thickness(margin), TimeSpan.FromMilliseconds(200))
+            {
+                EasingFunction = new QuadraticEase { EasingMode = EasingMode.EaseInOut }
+            };
+
+            this.BeginAnimation(WidthProperty, sizeAnimation);
+            this.BeginAnimation(HeightProperty, sizeAnimation);
+            this.BeginAnimation(MarginProperty, marginAnimation);
         }
     }
 }
