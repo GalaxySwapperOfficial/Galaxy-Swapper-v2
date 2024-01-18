c7dcf4e9c107b600a5188ba51812da53fb1a6933
Wslt <76235453+CodeWslt@users.noreply.github.com>
2024-01-18 16:49:48 -0500

    1.32
    Just a silent update nothing much changed.

--- Workspace/Global.cs
+++ Workspace/Global.cs
@@ -4,8 +4,8 @@ namespace Galaxy_Swapper_v2.Workspace
 {
     public static class Global
     {
-        public const string Version = "1.31";
-        public const string ApiVersion = "1.23";
+        public const string Version = "1.32";
+        public const string ApiVersion = "1.24";
         public static string Discord = "https://galaxyswapperv2.com/Discord.php";
         public static string Website = "https://galaxyswapperv2.com";
         public static string Download = "https://galaxyswapperv2.com/Downloads/InGame.php";
